e1699cca1bdb0a4e240c585a5582052d7a11fb78
Jake Felice <jasonfelice0@gmail.com>
2023-02-15 04:30:07 -0800

    Make square function efficient using memoization

--- square_memoization.js
+++ square_memoization.js
@@ -1,10 +1,15 @@
+const prevValue = [];
+
 const square = (n) => {
-    let result = 0;
-    for(let i = 0; i<n; i++) {
-      for(let j = 0; j<n; j++){
-          result += 1;
-      }
+  if(prevValue[n]){
+    return prevValue[n];
+  }
+  let result = 0;
+  for(let i = 0; i<n; i++) {
+    for(let j = 0; j<n; j++){
+        result += 1;
     }
-    return result;
-  };
-  
+  }
+  prevValue[n] = result;
+  return result;
+};
