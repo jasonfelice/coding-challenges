60464998c86c6739d77099d1fdaf4834bc5227a1
Jake Felice <jasonfelice0@gmail.com>
2023-02-16 23:38:44 -0800

    Use memoization in fibonacci function

--- fibonacci_memoization.js
+++ fibonacci_memoization.js
@@ -1,7 +1,13 @@
-const fib = (n) => {
+const fib = (n, prevValues = []) => {
+  if (prevValues[n]) {
+    return prevValues[n];
+  }
+  let result;
   if (n <= 2) {
     return 1;
   } else {
-    return fib(n - 1) + fib(n - 2);
+    result = fib(n - 1, prevValues) + fib(n - 2, prevValues);
   }
+  prevValues[n] = result;
+  return result;
 };
