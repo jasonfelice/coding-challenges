011900a2b7e1eacd67c05486d46e180579f7aadf
Jake Felice <jasonfelice0@gmail.com>
2023-02-19 02:00:52 -0800

    Log sum of k subarrays

--- subAr_sum.js
+++ subAr_sum.js
@@ -1,9 +1,18 @@
 const subArraySum = (array, k) => {
-  const result = [];
-  let sum;
-  for(let i = 0; i < k-1; i++) {
-    sum += array[i];
-    result.push(sum);
-  }
-  return result;
-};
+    const result = [];
+    let j = 0;
+    while (j <= array.length) {
+      let sum = 0;
+      for (let i = 0; i < k; i++) {
+        if(i+j === array.length) {
+            break;
+        }
+        sum += array[i+j];
+      }
+      result.push(sum);
+      j++;
+    }
+    return result;
+  };
+  
+  console.log(subArraySum([1,2,3,4,5,6], 3));
