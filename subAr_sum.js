c63d4e38b992e2c7a5f9d9586bf53283c0f6ade3
Jake Felice <jasonfelice0@gmail.com>
2023-02-20 02:26:15 -0800

    Sum of subarray of k length using sliding window

--- subAr_sum.js
+++ subAr_sum.js
@@ -1,18 +1,16 @@
 const subArraySum = (array, k) => {
     const result = [];
-    let j = 0;
-    while (j <= array.length) {
-      let sum = 0;
-      for (let i = 0; i < k; i++) {
-        if(i+j === array.length) {
-            break;
-        }
-        sum += array[i+j];
-      }
+    let start = 0;
+    let end = k-1;
+    let sum = array.slice(0, end+1).reduce((a, b) => a + b);
+    while (end < array.length-1) {
+      sum -= array[start];
+      start++;
+      end++;
+      sum += array[end];
       result.push(sum);
-      j++;
     }
     return result;
   };
-  
-  console.log(subArraySum([1,2,3,4,5,6], 3));
+    
+  console.log(subArraySum([1,2,3,4,5,6], 4));
