aaf8f60c029c5e98b22d7c5be4050c3b59f2fb11
Jake Felice <jasonfelice0@gmail.com>
2023-02-20 23:14:51 -0800

    Solve subarray using sliding window

--- subarray_division.js
+++ subarray_division.js
@@ -1,22 +1,15 @@
 function birthday(s, d, m) {
-    let num = s;
-    let nums = [];
-    let count = 0;
-    const add = (arr) => arr.reduce((a, b) => a + b, 0);
-    for (let i = 0; i < s.length; i++) {
-      let arrayElement = num.slice(0 + i, m + i);
-      nums.push(arrayElement);
-    }
-    if(num.length===1 && num[0]===d){
-        count++;
-    }else{
-      nums.forEach((el) => {
-          if (add(el) === d) {
-            count++;
-          }
-        });
-    }
-    
-  
-    return count;
+  const result = [];
+  let start = 0;
+  let end = m - 1;
+  let sum = s.slice(0, m).reduce((a ,b) => a + b);
+  result.push(sum);
+  while (end < s.length - 1) {
+    sum -= s[start];
+    start++;
+    end++;
+    sum += s[end];
+    result.push(sum);
+  }
+  return result.filter(sum => sum === d).length;
 }
