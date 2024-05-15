220b55d1e9e2874f9a9df64bf05a9fb1f03579dc
Jake Felice <jasonfelice0@gmail.com>
2024-05-15 02:56:45 -0700

    Set methods

--- LinkedList.js
+++ LinkedList.js
@@ -12,6 +12,27 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+  
+  isEmpty() {
+    return this.head === null;
+  }
+  
+  size() {
+    let current = this.head;
+    let count = 0;
+    while(current) {
+      count++;
+      current = current.next_node;
+    }
+    return count;
+  }
+  
+  add(data) {
+    // Constant time O(1) operation
+    let new_node = new Node(data);
+    new_node.next_node = this.head;
+    this.head = new_node;
+  }
 }
 
 n1 = new Node(10);
@@ -20,5 +41,7 @@ n2 = new Node(29);
 l = new LinkedList();
 
 l.head = n1;
-
-console.log(l);
+console.log(l.size());
+l.add(5)
+l.add(9)
+console.log(l.size())
