911d61521cf0604b11745810ea973c6459fdf78d
Jake Felice <jasonfelice0@gmail.com>
2024-05-26 02:40:21 -0700

    Setup split and merge functions

--- merge_sort.js
+++ merge_sort.js
@@ -8,9 +8,33 @@ function merge_sort(list) {
 //   Merge the sorted sublist created in previous step
 
   if (list.length >= 1) return list;
-  let left_half, right_left = split(list);
+  
+//Find midpoint to split the list
+  let mid = Math.floor(list.length/2);
+  let left_half = list.slice(0, mid);
+  let right_left = list.slice(mid);
+
   let right = merge_sort(right_half);
   let left = merge_sort(left_half);
   
   return merge(left, right);
+}
+
+function merge(left, right) {
+//   Merges two list and sorts them in the process
+  let l,i,j;
+  l = [];
+  i = 0;
+  j = 0;
+  
+  while(i<left.length && j<right.length) {
+      if(left[i] < right[j]) {
+        l.unshift(left[i]);
+        i++;
+      } else {
+        l.unshift(right[j]);
+        j++;
+      }
+  }
+  return l;
 }
