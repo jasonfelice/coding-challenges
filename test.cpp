efd580f0b531e351215cae4341f926b4f45a9a0c
Jake Felice <jasonfelice0@gmail.com>
2024-05-29 05:39:57 -0700

    Add c++ test

---
+++ test.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a,b,c;
+    a = 1;
+    b = 2;
+    c = a+b;
+    cout << "Hello World!" << c;
+    return 0;
+}
